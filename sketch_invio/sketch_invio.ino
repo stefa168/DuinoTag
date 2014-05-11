c6e2df561abe3b668bbed0f0aa370d6f4043e124
stefa168 <stefa168@hotmail.it>
2014-05-11 22:57:35 +0200

    First packets sending protocol and slight adjustments to the transmission

--- sketch_invio/sketch_invio.ino
+++ sketch_invio/sketch_invio.ino
@@ -1,19 +1,22 @@
 #define IR_HEAD 2000
-#define IR_SPACE 300
+#define IR_SPACE 600
 #define IR_ONE 1200
 #define IR_ZERO 600
 
-// BIT infos:
+#define SHOT_LENGTH 17
 
-byte datas[5][17] = { {1, 0,0,0,0,0,0,0, 0,0,0, ,,,,},
-                     {,,,,,,,,,,,,,,,,},
-                     {,,,,,,,,,,,,,,,,},
-                     {,,,,,,,,,,,,,,,,},
-                     {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
-                   }
+// BIT infos:
+byte datas[][17] = { 
+                   {1, 0,0,1,1,0,0,1,1, 1,0,1, 0,1,1,0, 1},
+                   {1, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0, 0},
+                   {1, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0, 0},
+                   {1, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0, 0},
+                   {1, 0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0, 0}
+                   };
 
 int IRledPin = 11;
-int run = 0;
+
+int count = 0;
 
 void setup(){
   pinMode(IRledPin, OUTPUT);
@@ -23,28 +26,39 @@ void setup(){
   
   Serial.begin(115200);
   Serial.println("Ready to send!");
+  delay(5000);
 }
 
 void loop(){
+  Serial.print("Code Number ");
+  Serial.println(count);
   digitalWrite(13, HIGH); // Let's just turn on the LED on the board to show that we are sending something!
   SendCode();
   digitalWrite(13, LOW);  // Now we turn off the LED.
-  delay(10000);
+  delay(6000);
+  count++;
 }
 
-void pulseIR(long microsecs) {
+void pulseIR(int microsecs) {
   cli();  // Let's stop any interrupt
   
-  while(microsecs > 0) {
+  while(microsecs >= 0) {
     digitalWrite(IRledPin, HIGH);  // To do this action 3 microseconds will pass.
-    delayMicroseconds(7); 
+    delayMicroseconds(11); 
     digitalWrite(IRledPin, LOW);  // Again other 3 microseconds
-    delayMicroseconds(7);
+    delayMicroseconds(11);
     
     // This number isn't exactly the sum of the 4 values up there; 
     // it's trimmed to make the timing of the receiver more accurate.
     // The best value that I noticed is about 29 microseconds.
-    microsecs -= 29;
+    
+    // EDIT: After using a Frequency Meter I found out that it is 
+    // better to transmit at about 11 microseconds of delay and
+    // to subtract exactly 28 from the counter. By doing this the 
+    // frequency is  almost exactly at 57 kHZ.
+    
+    // EDIT 2: Again, now the best time to subtract is 30 microsecs because it results in less erorrs
+    microsecs -= 30;
   }
   
   pauseIR(IR_SPACE);
@@ -70,18 +84,12 @@ void SendCode(){
   // stay the same for all the time.
   pulseIR(IR_HEAD);
   
-  // The code.
-  pulseIR(IR_ONE);
-  pulseIR(IR_ZERO);
-  pulseIR(IR_ONE);
-  
-  // I tried to do something useful, but for now we won't use it.
-  /*for(int i=0; i<sizeof(msg); i++){
-    switch(msg[i]){
-      case true:  pulseIR(IR_ONE);
-                 break;
-      case false: pulseIR(IR_ZERO);
-                 break;
+  for(uint8_t i=0; i<SHOT_LENGTH; i++) {
+    switch(datas[0][i]){
+      case 0: pulseIR(IR_ZERO);
+              break;
+      case 1: pulseIR(IR_ONE);
+              break;
     }
-  }*/
+  }
 }
--- sketch_ricezione/sketch_ricezione.ino
+++ sketch_ricezione/sketch_ricezione.ino
@@ -6,7 +6,7 @@
 #define IRpin       2
 
 // the maximum pulse we'll listen for - 65 milliseconds is a long time
-#define MAXPULSE 1500
+#define MAXPULSE 2500
 #define MAXPULSES 50
 
 // what our timing resolution should be, larger is better
@@ -15,31 +15,50 @@
 #define RESOLUTION 20
 
 // What percent we will allow in variation to match the same code
-#define FUZZINESS 20
+#define FUZZINESS 22
 
 #define IR_HEAD 2000
-#define IR_SPACE 300
+#define IR_SPACE 600
 #define IR_ONE 1200
 #define IR_ZERO 600
 
 // we will store up to MAXPULSES
-int data[MAXPULSES]; // pair is high and low pulse
-int numberpulses; // index for pulses we're storing
+uint16_t data[MAXPULSES]; // pair is high and low pulse
+uint8_t numberpulses; // index for pulses we're storing
+
+uint16_t highpulse, lowpulse;
+
+int count = 0;
+
+byte shotid;
+byte team;
+byte damages[] = {1,2,4,5,7,10,15,17,20,25,30,35,40,50,75,100};
+byte damage;
 
 void setup(void) {
   Serial.begin(115200);
+  Serial.println("Version 1.3 R1");
   Serial.println("Ready to decode IR!");
 }
 
 void loop(void) {
-  // Reset the number of pulses!
+
+  // Reset the number of pulses!  
   numberpulses = 0;
-  
-  
+  Serial.println();
+  Serial.print("Code Number ");
+  Serial.println(count);
+  count++;
   // If the code isn't correct or is a diffrent code (like one of a remote) we just delete all and return null so we restart.
-  if(listenForIR()){
-    Serial.println("An error occurred while converting.");
-    return;
+  switch(listenForIR()){
+    case 1: Serial.print("Header error: ");
+            Serial.println(lowpulse*RESOLUTION);
+            return;
+    case 2: Serial.print("No match error: ");
+            Serial.print(lowpulse*RESOLUTION);
+            Serial.print(" Pulses: ");
+            Serial.println(numberpulses);
+            return;
   }
   numberpulses--;
   Serial.print("New Signal: ");
@@ -49,8 +68,25 @@ void loop(void) {
   for(int i=0; i<numberpulses; i++){
     Serial.print(data[i]);
   }
-  
   Serial.println();
+  
+  if(data[0] == 1){
+    Serial.print("SHOT ID ");
+    shotid = data[1]*128 + data[2]*64 + data[3]*32 + data[4]*16 + data[5]*8 + data[6]*4 + data[7]*2 + data[8];
+    Serial.print(shotid);
+    Serial.print(" TEAM ");
+    team = data[9]*4 + data[10]*2 + data[11];
+    Serial.print(team);
+    Serial.print(" DAM ");
+    damage = data[12]*8 + data[13]*4 + data[14]*2 + data[15];
+    Serial.print(damages[damage]);
+    switch(data[16]){
+      case 0: Serial.println(" T 1 .");
+              break;
+      case 1: Serial.println(" T 2 .");
+              break;
+    }
+  }
 }
 
 
@@ -58,7 +94,7 @@ void loop(void) {
 int listenForIR(void) {
   // Let's keep counting until we don't return the number of pulses recorded.
   while (1) {
-    int highpulse, lowpulse;  // Temporary storage timing
+    //int highpulse, lowpulse;  // Temporary storage timing
     highpulse = lowpulse = 0; // Start out with no pulse length
  
 //  while (digitalRead(IRpin)) { // this is too slow!
@@ -73,7 +109,7 @@ int listenForIR(void) {
        // was received or the code is finished, so print what
        // we've grabbed so far, and then reset
        if ((highpulse >= MAXPULSE) && (numberpulses != 0)) {
-         return false;
+         return 0;
        }
     }
  
@@ -83,7 +119,7 @@ int listenForIR(void) {
        lowpulse++;
        delayMicroseconds(RESOLUTION);
        if ((lowpulse >= MAXPULSE)  && (numberpulses != 0)) {
-         return false;
+         return 0;
        }
     }
     
@@ -91,18 +127,18 @@ int listenForIR(void) {
     // ones so we get an array alerady complete
     // of all we need to work!
     
-    int oncode = lowpulse * RESOLUTION;
+    uint16_t oncode = lowpulse * RESOLUTION;
     
     if(numberpulses == 0){
       if(abs(oncode - IR_HEAD) > (oncode * FUZZINESS / 100)){
-        return true;
+        return 1;
       } 
     } else if(abs(oncode - IR_ONE) <= (oncode * FUZZINESS / 100)){
       data[numberpulses-1] = 1;
     } else if(abs(oncode - IR_ZERO) <= (oncode * FUZZINESS / 100)){
       data[numberpulses-1] = 0;
     } else {
-      return true;
+      return 2;
     }
  
     // we read one high-low pulse successfully, continue!
