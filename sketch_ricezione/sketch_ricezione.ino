7e32417a8ba5686878bd1b6df3cca493e5b91700
stefa168 <stefa168@hotmail.it>
2014-05-19 14:19:42 +0200

    Fixed some errors

--- sketch_ricezione/sketch_ricezione.ino
+++ sketch_ricezione/sketch_ricezione.ino
@@ -15,7 +15,7 @@
 #define RESOLUTION 20
 
 // What percent we will allow in variation to match the same code
-#define FUZZINESS 22
+#define FUZZINESS 23
 
 #define IR_HEAD 2000
 #define IR_SPACE 600
@@ -23,34 +23,62 @@
 #define IR_ZERO 600
 
 // we will store up to MAXPULSES
-uint16_t data[MAXPULSES]; // pair is high and low pulse
+uint16_t data[MAXPULSES+2]; // MAXPULSES + 2 because 1 pulse is the time before 
+                            // the start of the reading and the other one is the header
+                            
 uint8_t numberpulses; // index for pulses we're storing
 
 uint16_t highpulse, lowpulse;
 
+// Error 0 means no error.
+int error = 0;
+
+// State 0 means initialisation
+int state = 0;
+
 int count = 0;
 
-byte shotid;
-byte team;
+// Data of player
+// The UNique ID is used to identificate a tagger system. It must be unique!
+#define UN_ID 0
+
+// Data of shot packet.
+byte rec_shotid;
+byte rec_team;
+byte rec_damage;
+
 byte damages[] = {1,2,4,5,7,10,15,17,20,25,30,35,40,50,75,100};
-byte damage;
 
 void setup(void) {
   Serial.begin(115200);
   Serial.println("Version 1.3 R1");
   Serial.println("Ready to decode IR!");
+  
+  // Test for RGB output.
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+  analogWrite(5, 255);
+  delay(500);
+  analogWrite(5, 0);
+  analogWrite(6, 255);
+  delay(500);
+  analogWrite(6, 0);
+  analogWrite(9, 255);
+  delay(500);
+  analogWrite(9, 0);
+  // End of test.
+  
+  // State 1 = ended initialisation and starting to use StandByMode
+  state = 1;
 }
 
 void loop(void) {
-
-  // Reset the number of pulses!  
-  numberpulses = 0;
-  Serial.println();
-  Serial.print("Code Number ");
-  Serial.println(count);
-  count++;
+  
+  listenForIR();
+  
   // If the code isn't correct or is a diffrent code (like one of a remote) we just delete all and return null so we restart.
-  switch(listenForIR()){
+  switch(error){
     case 1: Serial.print("Header error: ");
             Serial.println(lowpulse*RESOLUTION);
             return;
@@ -60,6 +88,22 @@ void loop(void) {
             Serial.println(numberpulses);
             return;
   }
+  
+  switch(state){
+    case 0: state = 1; 
+            break;
+    case 1: ModeStandBy();
+            break;
+  }
+  
+  /*
+  // Reset the number of pulses!  
+  numberpulses = 0;
+  Serial.println();
+  Serial.print("Code Number ");
+  Serial.println(count);
+  count++;
+  
   numberpulses--;
   Serial.print("New Signal: ");
   Serial.print(numberpulses);
@@ -70,7 +114,7 @@ void loop(void) {
   }
   Serial.println();
   
-  if(data[0] == 1){
+  if(data[0] == 0){
     Serial.print("SHOT ID ");
     shotid = data[1]*64 + data[2]*32 + data[3]*16 + data[4]*8 + data[5]*4 + data[6]*2 + data[7];
     Serial.print(shotid);
@@ -86,12 +130,22 @@ void loop(void) {
       case 1: Serial.println(" T 2 .");
               break;
     }
-  }
+  } else {
+    
+  }*/
 }
 
+// StandByMode
+void ModeStandBy(){
+  Serial.println("STBYM");
+}
 
 // Function for IR listening. In a future version I might try to make all this multitasking so I can relax more.
-int listenForIR(void) {
+void listenForIR(void) {
+  // Reset the number of pulses.
+  numberpulses = 0;
+  // Reset the error.
+  error = 0;
   // Let's keep counting until we don't return the number of pulses recorded.
   while (1) {
     //int highpulse, lowpulse;  // Temporary storage timing
@@ -109,7 +163,7 @@ int listenForIR(void) {
        // was received or the code is finished, so print what
        // we've grabbed so far, and then reset
        if ((highpulse >= MAXPULSE) && (numberpulses != 0)) {
-         return 0;
+         return;
        }
     }
  
@@ -119,7 +173,7 @@ int listenForIR(void) {
        lowpulse++;
        delayMicroseconds(RESOLUTION);
        if ((lowpulse >= MAXPULSE)  && (numberpulses != 0)) {
-         return 0;
+         return;
        }
     }
     
@@ -131,14 +185,16 @@ int listenForIR(void) {
     
     if(numberpulses == 0){
       if(abs(oncode - IR_HEAD) > (oncode * FUZZINESS / 100)){
-        return 1;
+        error = 1;
+        return;
       } 
     } else if(abs(oncode - IR_ONE) <= (oncode * FUZZINESS / 100)){
       data[numberpulses-1] = 1;
     } else if(abs(oncode - IR_ZERO) <= (oncode * FUZZINESS / 100)){
       data[numberpulses-1] = 0;
     } else {
-      return 2;
+      error = 2;
+      return;
     }
  
     // we read one high-low pulse successfully, continue!
