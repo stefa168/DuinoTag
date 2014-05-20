5865338bf450652603bc7dab68b21b5e2775f7d3
stefa168 <stefa168@hotmail.it>
2014-05-20 16:24:07 +0200

    New functions and reduced code a little.

--- new_send_sketch/new_send_sketch.ino
+++ new_send_sketch/new_send_sketch.ino
@@ -1,3 +1,5 @@
+//#define DEBUG
+
 #include <IRremote.h>
 
 IRsend irsend;
@@ -11,6 +13,7 @@ byte damages[] = {1, 2, 4, 5, 7, 10, 15, 17, 20, 25, 30, 35, 40, 50, 75, 100};
 
 
 unsigned long dataToSend;
+unsigned int dataToSendLength;
 
 
 void setup()
@@ -21,95 +24,107 @@ void setup()
 
 void loop() {
   
-  Serial.println("Shot command:");
-  while(1){
-    Serial.println("Insert Player ID (MUST be between 0 and 127)");
-    waitForInput();
-    tempPlayerID = Serial.parseInt();
-    if(tempPlayerID <= 127 && tempPlayerID >= 0){
-      Serial.print(tempPlayerID);
-      Serial.println(": OK.");
-      break;
-    }
-  }
-  
-  while(1){
-    Serial.println("Insert Team ID (MUST be between 0 and 7)");
-    waitForInput();
-    tempPlayerTeam = Serial.parseInt();
-    if(tempPlayerTeam <= 7 && tempPlayerID >= 0){
-      Serial.print(tempPlayerTeam);
-      Serial.println(": OK.");
-      break;
-    }
-  }
-  
-  Serial.println("Damages");
-  
-  for(int i=0; i<sizeof(damages); i++){
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.println(damages[i]);
-  }
-  
-  while(1){
-    Serial.println("Insert Damage ID (MUST be between 0 and 15)");
-    waitForInput();
-    tempDamageID = Serial.parseInt();
-    if(tempDamageID <= 15 && tempPlayerID >= 0){
-      // We just save the correct damage.
-      Serial.print(tempDamageID);
-      Serial.println(": OK.");
-      break;
-    }
-  }
-  
   // Reset the data to send
-  dataToSend = 0;
-  
-  //Start with the PlayerID
-  for(int i=0; i<7; i++){
-    if(tempPlayerID & 0x40){
-      dataToSend = (dataToSend << 1) | 1;
-    } else {
-     dataToSend <<= 1;
+  resetDataToSend();
+  Serial.println("Shot or Command");
+  int typeOfCommand = waitForInput();
+  if(typeOfCommand == 'S' || typeOfCommand && 's'){
+    Serial.println("Shot command:");
+    while(1){
+      Serial.println("Insert Player ID (MUST be between 0 and 127)");
+      tempPlayerID = waitForIntInput();
+      if(tempPlayerID <= 127 && tempPlayerID >= 0){
+        addDataToSend(7, tempPlayerID, 0x40, true); // 0x40 == 0b 0100 0000 (7 bits)
+        Serial.print(tempPlayerID);
+        Serial.println(": OK.");
+        break;
+      }
     }
-    Serial.println(dataToSend, BIN);
-    tempPlayerID <<= 1;
-  }
-  
-  // Now it's the time for TeamID
-  for(int i=0; i<3; i++){
-    if(tempPlayerTeam & 0x4){
-      dataToSend = (dataToSend << 1) | 1;
-    } else {
-     dataToSend <<= 1;
+    
+    while(1){
+      Serial.println("Insert Team ID (MUST be between 0 and 7)");
+      tempPlayerTeam = waitForIntInput();
+      if(tempPlayerTeam <= 3 && tempPlayerTeam >= 0){ 
+        addDataToSend(3, tempPlayerTeam, 0x4, true); // 0x4 == 0b 100
+        Serial.print(tempPlayerTeam);
+        Serial.println(": OK.");
+        break;
+      }
     }
-    Serial.println(dataToSend, BIN);
-    tempPlayerTeam <<= 1;
-  }
-  
-  // Finally the damage.
-  for(int i=0; i<4; i++){
-    if(tempDamageID & 0x8){
-      dataToSend = (dataToSend << 1) | 1;
-    } else {
-     dataToSend <<= 1;
+    
+    Serial.println("Damages");
+    
+    for(int i=0; i<sizeof(damages); i++){
+      Serial.print(i);
+      Serial.print(": ");
+      Serial.println(damages[i]);
     }
-    Serial.println(dataToSend, BIN);
-    tempDamageID <<= 1;
+    
+    while(1){
+      Serial.println("Insert Damage ID (MUST be between 0 and 15)");
+      tempDamageID = waitForIntInput();
+      if(tempDamageID <= 15 && tempDamageID >= 0){
+        addDataToSend(4, tempDamageID, 0x8, true); // 0x8 == 0b 1000
+        Serial.print(tempDamageID);
+        Serial.println(": OK.");
+        break;
+      }
+    }  
+    
+    digitalWrite(13, HIGH);
+    irsend.sendDtag(dataToSend, 16, 38);
+    digitalWrite(13, LOW);
+    Serial.println("Sent.\n");
+    
+  } else if (typeOfCommand == 'S' || typeOfCommand && 's'){
+    Serial.println("\nCommand.");
+    for(int i=0x80; i<=0x8C; i++){
+      Serial.println(i, HEX);
+    }
   }
-  
-  
-  digitalWrite(13, HIGH);
-  irsend.sendDtag(dataToSend, 16, 38);
-  digitalWrite(13, LOW);
-  Serial.println("Sent.\n");
   
 }
 
-void waitForInput(){
+void resetDataToSend(){
+  dataToSend = 0;
+  dataToSendLength = 0;
+  #ifdef DEGBUG
+  Serial.println("Reset dataToSend");
+  #endif
+}
+
+void addDataToSend(int bits, int value, int topBit, boolean autoIncrement){
+  for(int i=0; i<bits; i++){
+    if(value & topBit){
+      dataToSend = (dataToSend << 1) | 1;
+    } else {
+     dataToSend <<= 1;
+    }
+    #ifdef DEBUG
+    Serial.println(dataToSend, BIN);
+    #endif
+    value <<= 1;
+  }
+  if(autoIncrement){
+    dataToSendLength += bits;
+    #ifdef DEBUG
+    Serial.print("\nAdded ");
+    Serial.print(bits);
+    Serial.println(" bits to the stack.");
+    #endif
+  }
+}
+
+int waitForInput(){
   while(Serial.available() == 0){
     // Just wait.
   }
+  return Serial.read();
+}
+
+int waitForIntInput(){
+  while(Serial.available() == 0){
+    // Just wait.
+  }
+  return Serial.parseInt();
 }
